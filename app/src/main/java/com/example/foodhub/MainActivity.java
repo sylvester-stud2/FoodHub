29e6b9493fe3cd56548e32a57258b25d8953274e
Moloko <152928508+sylvester-stud2@users.noreply.github.com>
2024-05-03 19:22:10 +0200

    buttons

--- app/src/main/java/com/example/foodhub/MainActivity.java
+++ app/src/main/java/com/example/foodhub/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
+
+
 }
