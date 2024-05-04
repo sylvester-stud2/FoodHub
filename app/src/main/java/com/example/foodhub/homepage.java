672fc29b3d17c1a0a46b8945faf23f931253cf2e
Moloko <152928508+sylvester-stud2@users.noreply.github.com>
2024-05-04 12:00:03 +0200

    Final-yet to be decided design of the logins

---
+++ app/src/main/java/com/example/foodhub/homepage.java
@@ -0,0 +1,15 @@
+package com.example.foodhub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class homepage extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        new_page is just new_page.xml
+        setContentView(R.layout.homepage);
+    }
+}
