175526b289a7a816af3bd940cb002dcedcb69423
Gwen Ledile <ledilegwen@gmail.com>
2024-05-11 15:24:22 +0200

    gwen changes

--- app/src/main/java/com/example/foodhub/MainActivity.java
+++ app/src/main/java/com/example/foodhub/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.foodhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -74,6 +75,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
 
 }
